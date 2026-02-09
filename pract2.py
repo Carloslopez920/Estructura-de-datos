91934e2121fcb4d900e73981a48029f15353362d
Carloslopez <carloslopessierra112@gmail.com>
2026-02-09 10:50:13 -0600

    pract2.py

--- pract2.py
+++ pract2.py
@@ -2,17 +2,17 @@ import tkinter as tk
 from tkinter import simpledialog
 
 def estatica():
-    calificaciones = [0] * 5  # Equivalente a: int calificaciones[] = new int[5];
+    calificaciones = [0] * 5  
     
     root = tk.Tk()
     root.withdraw()
     
-    for i in range(5):  # Equivalente a: for(int i = 0; i < 5; i++)
+    for i in range(5): 
         entrada = simpledialog.askstring("Input", "Captura la calificación")
         if entrada is not None:
             calificaciones[i] = int(entrada)
     
-    for i in range(5):  # Equivalente a: for(int i = 0; i < 5; i++)
+    for i in range(5): 
         print("La calificación " + str(i+1) + " es: " + str(calificaciones[i]))
    
 def dinamica():
@@ -29,4 +29,5 @@ def dinamica():
 
 if __name__ == "__main__":
     estatica()  
+
     dinamica()   
